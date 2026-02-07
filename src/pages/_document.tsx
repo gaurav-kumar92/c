81e9027e724d2770d15eb2d2f6313383dd037051
gaurav kumar <gauravkumar92@gmail.com>
2026-02-08 00:44:05 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
       <script src="https://pl28641636.effectivegatecpm.com/00/fd/2d/00fd2d287f4e960073648f80a79ab1e7.js"></script>
-        <meta name="google-adsense-account" content="ca-pub-9788212864829344"></meta>
+       
       </Head>
       <body className="antialiased">
         <Main />
